fd4d88880321a1e4b18920acf0f4d2b700e257b5
Ram Roy <ramroycombd@gmail.com>
2024-11-05 22:17:35 +0600

    first commit

---
+++ client/src/layouts/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+  return (
+    <div>
+      {/* Navbar */}
+
+      {/* <Outlet */}
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Root;
