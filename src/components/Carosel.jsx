eeb12a978df074d59a494b5d7a180c3d94621759
Ram Roy <ramroycombd@gmail.com>
2024-11-09 10:44:09 +0600

    tab added

--- src/components/Carosel.jsx
+++ src/components/Carosel.jsx
@@ -6,6 +6,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import bg1 from '../assets/images/bg1.jpg';
+
+import bg2 from '../assets/images/bg2.jpg';
+import bg3 from '../assets/images/bg3.jpg';
+import bg4 from '../assets/images/bg4.jpg';
+import bg5 from '../assets/images/bg5.jpg';
+import bg6 from '../assets/images/bg6.jpg';
 
 
 // import required modules
@@ -14,7 +21,7 @@ import Slide from './Slide';
 
 export default function Carosel() {
   return (
-    <>
+    <div className='container px-5 py-10 mx-auto'>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -31,16 +38,14 @@ export default function Carosel() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide> <Slide></Slide> </SwiperSlide>
-        <SwiperSlide> <Slide></Slide> </SwiperSlide>
-        <SwiperSlide> <Slide></Slide> </SwiperSlide>
-        <SwiperSlide> <Slide></Slide> </SwiperSlide>
-        <SwiperSlide> <Slide></Slide> </SwiperSlide>
-        <SwiperSlide> <Slide></Slide> </SwiperSlide>
-        <SwiperSlide> <Slide></Slide> </SwiperSlide>
-    
-    
+        <SwiperSlide> <Slide image={bg1} text="this is slide 1"
+        ></Slide> </SwiperSlide>
+        <SwiperSlide> <Slide image={bg2}></Slide> </SwiperSlide>
+        <SwiperSlide> <Slide image={bg3}></Slide> </SwiperSlide>
+        <SwiperSlide> <Slide image={bg4}></Slide> </SwiperSlide>
+        <SwiperSlide> <Slide image={bg5}></Slide> </SwiperSlide>
+        <SwiperSlide> <Slide image={bg6}></Slide> </SwiperSlide>  
       </Swiper>
-    </>
+    </div>
   );
 }
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,21 +1,19 @@
 
 
-const Slide = () => {
+const Slide = ({image, text}) => {
+
     return (
         <div
   className="hero h-[600px]"
   style={{
-    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+    backgroundImage: "url(" + image + ")",
   }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+     {text}
       </p>
-      <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
