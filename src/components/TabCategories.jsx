ef2ab6a480599385440592d52fcd98c8c954307b
Ram Roy <ramroycombd@gmail.com>
2024-11-09 11:05:48 +0600

    card added

--- src/components/TabCategories.jsx
+++ src/components/TabCategories.jsx
@@ -1,28 +1,41 @@
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import JobsCard from "./JobsCard";
 
 const TabCategories = () => {
-    return (
-        <div>  <Tabs>
-        <TabList>
-          <Tab>Web Development</Tab>
-          <Tab>Graphics Design</Tab>
-          <Tab>Digital Marketing</Tab>
-        </TabList>
-    
-        <TabPanel>
-          <h2>Any content 1</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 3</h2>
-        </TabPanel>
-      </Tabs>
+  return (
+    <div>
+      {" "}
+      <Tabs>
+        <h1 className="text-center mb-5 font-bold text-3xl">
+          Browse the Jobs by Categories
+        </h1>
+        <p className="text-center mb-5">
+          Quickly syndicate dynamic resources for interoperable manufactured
+          products. Authoritatively <br /> productize interoperable resources
+          and visionary potentialities.
+        </p>
+        <div className="container px-5 mx-auto">
+          <div className="flex justify-center items-center">
+            <TabList>
+              <Tab>Web Development</Tab>
+              <Tab>Graphics Design</Tab>
+              <Tab>Digital Marketing</Tab>
+            </TabList>
+          </div>
+          <TabPanel>
+            <JobsCard></JobsCard>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 3</h2>
+          </TabPanel>
         </div>
-    );
+      </Tabs>
+    </div>
+  );
 };
 
 export default TabCategories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Carosel from "../components/Carosel";
+import TabCategories from "../components/TabCategories";
 
 const Home = () => {
   return (
     <div>
       <h1>This is home</h1>
       <Carosel></Carosel>
+      <TabCategories></TabCategories>
     </div>
   );
 };
